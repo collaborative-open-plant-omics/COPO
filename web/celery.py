594f1d999a7e71db21cd47383493681779df1f3e
aliceminotto <i.m.e@hotmail.it>
2021-07-14 14:44:15 +0100

    reverted celery task for awaiting tolid to a sensible time window

--- web/celery.py
+++ web/celery.py
@@ -39,7 +39,7 @@ app.conf.beat_schedule = {
     },
     'poll_missing_tolids': {
         'task': 'web.apps.web_copo.tasks.poll_missing_tolids',
-        'schedule': timedelta(seconds=60)  # leave time for human creation of tolid
+        'schedule': timedelta(hours=12)  # leave time for human creation of tolid
     },
     'poll_expired_viewlocks': {
         'task': 'web.apps.web_copo.tasks.poll_expired_viewlocks',
