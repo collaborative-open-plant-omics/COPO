018614167e6e1772c3dda7076c4e54719ade5579
aliceminotto <i.m.e@hotmail.it>
2021-09-07 17:02:46 +0100

    prevent duplicated profile titles

--- dal/copo_da.py
+++ dal/copo_da.py
@@ -1776,6 +1776,10 @@ class Profile(DAComponent):
         p = self.get_record(ObjectId(profile_id))
         return p["title"]
 
+    def get_by_title(self, title):
+        p = self.get_collection_handle().find({"title" : title})
+        return cursor_to_list(p)
+
 
 class CopoGroup(DAComponent):
     def __init__(self):
