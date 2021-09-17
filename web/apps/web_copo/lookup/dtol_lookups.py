5bd6337b2c3f78a36836ca1cdd3a1265009a0a04
aliceminotto <i.m.e@hotmail.it>
2021-09-17 14:08:07 +0100

    added BC labels

--- web/apps/web_copo/lookup/dtol_lookups.py
+++ web/apps/web_copo/lookup/dtol_lookups.py
@@ -846,7 +846,17 @@ DTOL_ENUMS = {
         "TK Community Use Only",
         "NOT_APPLICABLE",
         "NOT_PROVIDED",
-        "NOT_COLLECTED"
+        "NOT_COLLECTED",
+        "BC Clan",
+        "BC Provenance",
+        "BC Multiple Communities",
+        "BC Outreach",
+        "BC Consent Verified",
+        "BC Consent Non-Verified",
+        "BC Research Use",
+        "BC Open to Commercialization",
+        "BC Non-Commercial",
+        "BC Open to Collaboration"
     ],
     "ETHICS_PERMIT_MANDATORY" : [
         "Y",
