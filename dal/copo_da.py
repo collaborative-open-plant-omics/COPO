81b2a4fe638b0219c51f64f742f0c83400b1bf7f
aliceminotto <i.m.e@hotmail.it>
2021-09-24 13:11:49 +0100

    better log message

--- dal/copo_da.py
+++ dal/copo_da.py
@@ -1080,7 +1080,7 @@ class Submission(DAComponent):
                 # submission retry time has elapsed so re-add to list
                 out.append(s)
                 self.update_submission_modified_timestamp(s["_id"])
-                lg.log("ADDING STALLED SUBMISSION BACK INTO QUEUE - copo_da:912", level=Loglvl.ERROR, type=Logtype.FILE)
+                lg.log("ADDING STALLED SUBMISSION " + str(s["_id"]) + "BACK INTO QUEUE - copo_da:1083", level=Loglvl.ERROR, type=Logtype.FILE)
 
                 # no need to change status
             elif s.get("dtol_status", "") == "pending":
