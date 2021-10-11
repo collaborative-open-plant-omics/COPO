b9e9c37c4d1ec0ae662a29c33261cf8f1cf182ad
aliceminotto <i.m.e@hotmail.it>
2021-10-11 14:04:02 +0100

    change allowed_hosts and cors for portability

--- web/settings/base.py
+++ web/settings/base.py
@@ -23,16 +23,12 @@ LOGIN_URL = '/accounts/auth/'
 DEBUG = True if str(resolve_env.get_env('DEBUG')).lower() == 'true' else False
 
 # ALLOWED_HOSTS = [ gethostname(), gethostbyname(gethostname()), ]
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '.copo-project.org',
-                 '.demo.copo-project.org', 'localhost']
+ALLOWED_HOSTS = [x.strip() for x in os.getenv("ALLOWED_HOSTS").split(',')]
 ALLOWED_CIDR_NETS = ['10.0.0.0/24']
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://0.0.0.0:8000",
-    "http://0.0.0.0:80",
-    "http://127.0.0.1:8000",
     "https://copo-project.org",
-    "http://demo.copo-project.org"
+    "http://demo.copo-project.org",
+    "http://copodev.cyverseuk.org"
 ]
 DEBUG_PROPAGATE_EXCEPTIONS = True
 # Django's base applications definition
