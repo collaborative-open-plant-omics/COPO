43f019f31175848707a166af9a6e389b8e914f43
aliceminotto <i.m.e@hotmail.it>
2021-10-28 16:01:27 +0100

    add barcoding mapping file

---
+++ web/apps/web_copo/lookup/dtol_barcoding.py
@@ -0,0 +1,31 @@
+ENA_MAPPING = {
+    #barcoding loci to ENA marker sequence checklist
+    "ITS" : "ITS rDNA",
+    "rbcL" : "Single CDS genomic DNA",
+    "trnL" : "",
+    "ITS2" : "ITS rDNA",
+    "psbA-trnH" : "Multi-Locus Marker",
+    "COI" : "COI gene",
+    "16S" : "rRNA Gene",
+    "CO1" : "COI gene",
+    "18S (V4, V9)" : "",
+    "LSU D2/D3" : "rRNA Gene",
+    "tufA" : "Single CDS genomic DNA",
+    "psaA" : "Single CDS genomic DNA",
+    "18S V4" : "rRNA Gene",
+    "18S V9" : "rRNA Gene",
+    "18S" : "rRNA Gene"
+
+}
+
+CHECKLISTS = {
+    "rRNA gene" : {
+        "mandatory" : [
+            "organism"
+        ],
+        "optional" : []
+    },
+    "Single CDS genomic DNA" : {
+
+    }
+}
