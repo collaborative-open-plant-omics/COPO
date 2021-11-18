1c26fa371f574e4c0a4dd1ee7c1eb2c484d3d621
shaw2thefloor <shaw2thefloor@gmail.com>
2021-11-18 17:10:31 +0000

    testing

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,13 +1,14 @@
 from selenium.webdriver.common.by import By
 
 
-def prone_of_these_elements_is_visible(element1, element2):
+def one_of_these_elements_is_visible(element1, element2):
     def wait_for_condition(driver):
         attribute = driver.find_element(By.ID, element1)
         if attribute.is_displayed():
             return attribute
         elif driver.find_element(By.ID, element2).is_displayed():
             return driver.find_element(By.ID, element2)
+
         else:
             return False
 
