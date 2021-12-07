8101b654f9eb5b278dbf3d4bf024e06314fb8290
shaw2thefloor <shaw2thefloor@gmail.com>
2021-12-07 17:05:05 +0000

    required column validator working

--- web/apps/web_copo/utils/EnaSpreadsheetParse.py
+++ web/apps/web_copo/utils/EnaSpreadsheetParse.py
@@ -1,18 +1,53 @@
-from django.http import HttpResponse
-from submission.helpers.generic_helper import notify_frontend
+import inspect
+import math
+import os
+import uuid
+from os.path import join, isfile
+from pathlib import Path
+from shutil import rmtree
+from urllib.error import HTTPError
+
+import jsonpath_rw_ext as jp
+import pandas
+from django.conf import settings
+from django.core.files.storage import default_storage
 from django_tools.middlewares import ThreadLocal
+from exceptions_and_logging import logger
+from web.apps.web_copo.lookup import dtol_lookups as lkup
+import web.apps.web_copo.schemas.utils.data_utils as d_utils
+from api.utils import map_to_dict
+from dal.copo_da import Sample, DataFile, Profile
+from submission.helpers.generic_helper import notify_frontend
+from web.apps.web_copo.copo_email import CopoEmail
+from web.apps.web_copo.lookup import dtol_lookups as lookup
+from web.apps.web_copo.lookup import lookup as lk
+from web.apps.web_copo.lookup.lookup import SRA_SETTINGS
+from web.apps.web_copo.schemas.utils.data_utils import json_to_pytype
+from web.apps.web_copo.utils.dtol.Dtol_Helpers import query_public_name_service
+from django.http import HttpResponse
+from web.apps.web_copo.validators.tol_validators import optional_field_dtol_validators as optional_validators, \
+    taxon_validators
+from web.apps.web_copo.validators.ena_validators import required_field_ena_seq_validators as required_validators
+from web.apps.web_copo.validators.validator import Validator
 
+l = logger.Logger("exceptions_and_logging/logs")
 
-# from .tol_validators import optional_field_dtol_validators as optional_validators
 
 def parse_ena_spreadsheet(request):
+    # method called by rest
     file = request.FILES["file"]
     name = file.name
     ena = ENASpreadsheet(file=file)
     if name.endswith("xlsx") or name.endswith("xls"):
         fmt = 'xls'
-    elif name.endswith("csv"):
-        fmt = 'csv'
+    else:
+        return HttpResponse(status=415, content="Please make sure your manifest is in xlxs format")
+
+    if ena.loadManifest(fmt):
+        l.log("Dtol manifest loaded")
+        if ena.validate():
+            l.log("About to collect Dtol manifest")
+            # dtol.collect()
 
     return HttpResponse()
 
@@ -22,17 +57,11 @@ class ENASpreadsheet:
     def __init__(self, file):
         self.req = ThreadLocal.get_current_request()
         self.profile_id = self.req.session.get("profile_id", None)
-        sample_images = Path(settings.MEDIA_ROOT) / "sample_images"
-        display_images = Path(settings.MEDIA_ROOT) / "img" / "sample_images"
-        self.these_images = sample_images / self.profile_id
-        self.display_images = display_images / self.profile_id
+
         self.data = None
+        self.fields = None
         self.required_field_validators = list()
-        self.optional_field_validators = list()
-        self.taxon_field_validators = list()
-        self.optional_validators = optional_validators
-        self.required_validators = required_validators
-        self.taxon_validators = taxon_validators
+
         self.symbiont_list = []
         self.validator_list = []
         # if a file is passed in, then this is the first time we have seen the spreadsheet,
@@ -56,20 +85,9 @@ class ENASpreadsheet:
         required = dict(globals().items())["required_validators"]
         for element_name in dir(required):
             element = getattr(required, element_name)
-            if inspect.isclass(element) and issubclass(element, TolValidtor) and not element.__name__ == "Validator":
+            if inspect.isclass(element) and issubclass(element, Validator) and not element.__name__ == "Validator":
                 self.required_field_validators.append(element)
-        # create list of optional validators
-        optional = dict(globals().items())["optional_validators"]
-        for element_name in dir(optional):
-            element = getattr(optional, element_name)
-            if inspect.isclass(element) and issubclass(element, TolValidtor) and not element.__name__ == "Validator":
-                self.optional_field_validators.append(element)
-        # create list of taxon validators
-        optional = dict(globals().items())["taxon_validators"]
-        for element_name in dir(optional):
-            element = getattr(optional, element_name)
-            if inspect.isclass(element) and issubclass(element, TolValidtor) and not element.__name__ == "Validator":
-                self.taxon_field_validators.append(element)
+
 
     def loadManifest(self, m_format):
 
@@ -99,3 +117,57 @@ class ENASpreadsheet:
                                 html_id="sample_info")
                 return False
             return True
+
+    def validate(self):
+        flag = True
+        errors = []
+        warnings = []
+        self.isupdate = False
+
+        try:
+            # get definitive list of mandatory DTOL fields from schema
+            s = json_to_pytype(lk.WIZARD_FILES["ena_seq_manifest"], compatibility_mode=False)
+            self.fields = jp.match(
+                '$.properties[?(@.specifications[*] == "ena_seq" & @.required=="true")].versions[0]',
+                s)
+
+            # validate for required fields
+            for v in self.required_field_validators:
+                errors, warnings, flag, self.isupdate = v(profile_id=self.profile_id, fields=self.fields,
+                                                          data=self.data,
+                                                          errors=errors, warnings=warnings, flag=flag,
+                                                          isupdate=self.isupdate).validate()
+
+            # send warnings
+            if warnings:
+                notify_frontend(data={"profile_id": self.profile_id},
+                                msg="<br>".join(warnings),
+                                action="warning",
+                                html_id="warning_info2")
+            # if flag is false, compile list of errors
+            if not flag:
+                errors = list(map(lambda x: "<li>" + x + "</li>", errors))
+                errors = "".join(errors)
+
+                notify_frontend(data={"profile_id": self.profile_id},
+                                msg="<h4>" + self.file.name + "</h4><ol>" + errors + "</ol>",
+                                action="error",
+                                html_id="sample_info")
+                return False
+
+
+
+        except Exception as e:
+            error_message = str(e).replace("<", "").replace(">", "")
+            notify_frontend(data={"profile_id": self.profile_id}, msg="Server Error - " + error_message,
+                            action="info",
+                            html_id="sample_info")
+            return False
+
+        # if we get here we have a valid spreadsheet
+        notify_frontend(data={"profile_id": self.profile_id}, msg="Spreadsheet is Valid", action="info",
+                        html_id="sample_info")
+        notify_frontend(data={"profile_id": self.profile_id}, msg="", action="close", html_id="upload_controls")
+        notify_frontend(data={"profile_id": self.profile_id}, msg="", action="make_valid", html_id="sample_info")
+
+        return True
