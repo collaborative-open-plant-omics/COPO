d242dd8c7c4bd5cf500a35bf2de20e1a1b4de7e5
shaw2thefloor <shaw2thefloor@gmail.com>
2022-01-07 16:57:44 +0000

    celery task retrieving and deserialising manifest data from mongo

--- web/apps/web_copo/utils/ajax_handlers.py
+++ web/apps/web_copo/utils/ajax_handlers.py
@@ -1367,16 +1367,12 @@ def sample_spreadsheet(request):
         l.log("Dtol manifest loaded", type=Logtype.FILE)
         srlz_dtol = pickle.dumps(dtol.file)
         p_id = request.session["profile_id"]
-        r = {"manifest_data": srlz_dtol, "profile_id": p_id, "schema_validation_status": "pending",
-             "taxon_validation_status": "pending", "err_msg": [],
-             "time_added": datetime.utcnow(),
-             "times_validated": 0
-             }
-        ValidationQueue().get_collection_handle().insert_one(r)
-        # make_validation_record(p_id, srlz_dtol)
-        # if dtol.validate_taxonomy() and dtol.validate():
-        #    l.log("About to collect Dtol manifest", type=Logtype.FILE)
-        #    dtol.collect()
+        r = {"$set": {"manifest_data": srlz_dtol, "profile_id": p_id, "schema_validation_status": "pending",
+                      "taxon_validation_status": "pending", "err_msg": [],
+                      "time_added": datetime.utcnow(),
+                      }}
+        ValidationQueue().get_collection_handle().update_one({"profile_id": p_id}, r, upsert=True)
+
     return HttpResponse()
 
     '''
--- web/apps/web_copo/validators/validation_celery_handler.py
+++ web/apps/web_copo/validators/validation_celery_handler.py
@@ -1,7 +1,54 @@
 from dal.copo_da import ValidationQueue
+from web.apps.web_copo.validators.tol_validators import optional_field_dtol_validators as optional_validators, \
+    taxon_validators
+from web.apps.web_copo.validators.tol_validators import required_field_dtol_validators as required_validators
+from web.apps.web_copo.validators.validator import Validator
+import pandas
+import inspect
+import pickle
+from web.apps.web_copo.lookup import dtol_lookups as lookup
 
 
-def process_validation_queue():
-    # get all manifests queued for validation
-    m_list = ValidationQueue().get_queued_manifests()
-    print(m_list)
+class ProcessValidationQueue:
+
+    def __init__(self):
+        self.required_field_validators = list()
+        self.optional_field_validators = list()
+        self.optional_field_validators = list()
+        self.taxon_field_validators = list()
+        self.optional_validators = optional_validators
+        self.required_validators = required_validators
+        self.taxon_validators = taxon_validators
+        self.symbiont_list = []
+        self.validator_list = []
+        self.sample_data = None
+
+    def process_validation_queue(self):
+        # get all manifests queued for validation
+        # create list of required validators
+        required = dict(globals().items())["required_validators"]
+        for element_name in dir(required):
+            element = getattr(required, element_name)
+            if inspect.isclass(element) and issubclass(element, Validator) and not element.__name__ == "Validator":
+                self.required_field_validators.append(element)
+        # create list of optional validators
+        optional = dict(globals().items())["optional_validators"]
+        for element_name in dir(optional):
+            element = getattr(optional, element_name)
+            if inspect.isclass(element) and issubclass(element, Validator) and not element.__name__ == "Validator":
+                self.optional_field_validators.append(element)
+        # create list of taxon validators
+        optional = dict(globals().items())["taxon_validators"]
+        for element_name in dir(optional):
+            element = getattr(optional, element_name)
+            if inspect.isclass(element) and issubclass(element, Validator) and not element.__name__ == "Validator":
+                self.taxon_field_validators.append(element)
+
+        queued_manifests = ValidationQueue().get_queued_manifests()
+
+        for qm in queued_manifests:
+            self.sample_data = pickle.loads(qm["manifest_data"])
+            try:
+                self.data = pandas.read_excel(self.sample_data, keep_default_na=False, na_values=lookup.NA_VALS)
+            except:
+                pass
