ecce472951ed44e606f0a02f909b65775f4383d5
shaw2thefloor <shaw2thefloor@gmail.com>
2022-01-17 15:46:15 +0000

    refactoring update code to work with celery - 2

--- web/apps/web_copo/validators/validation_celery_handler.py
+++ web/apps/web_copo/validators/validation_celery_handler.py
@@ -1,4 +1,4 @@
-from dal.copo_da import ValidationQueue, Profile
+from dal.copo_da import ValidationQueue, Profile, Sample
 from web.apps.web_copo.validators.tol_validators import optional_field_dtol_validators as optional_validators, taxon_validators
 from web.apps.web_copo.validators.tol_validators import required_field_dtol_validators as required_validators
 from web.apps.web_copo.validators.validator import Validator
@@ -12,7 +12,7 @@ from urllib.error import HTTPError
 from web.apps.web_copo.schemas.utils.data_utils import json_to_pytype
 from web.apps.web_copo.lookup import lookup as lk
 import jsonpath_rw_ext as jp
-
+from api.utils import map_to_dict
 
 class ProcessValidationQueue:
 
@@ -32,6 +32,7 @@ class ProcessValidationQueue:
         self.data = None
         self.isupdate = None
         self.type = None
+        self.public_name_list = list()
 
     def process_validation_queue(self):
         """This method is called from celery and looks in the validationqueue collection in mongo for manifests which need validation
@@ -220,11 +221,11 @@ class ProcessValidationQueue:
             ValidationQueue().set_schema_validation_complete(qm["_id"])
 
             if self.isupdate:
-
+                self.make_update_notifications(qm)
             else:
-                self.make_table()
+                self.make_table(qm)
 
-    def make_table(self):
+    def make_table(self, qm):
         notify_frontend(data={"profile_id": self.profile_id}, msg="Spreadsheet is Valid", action="info",
                         html_id="sample_info")
         notify_frontend(data={"profile_id": self.profile_id}, msg="", action="close", html_id="upload_controls")
@@ -242,16 +243,52 @@ class ProcessValidationQueue:
                 if x is math.nan:
                     r[idx] = ""
             sample_data.append(r)
-        # store sample data in the session to be used to create mongo objects
-        # self.req.session["sample_data"] = sample_data
-        # self.req.session["isupdate"] = self.isupdate
-        # if self.isupdate:
-        #    DtolSpreadsheet().detect_updates()
 
-        # else:
         notify_frontend(data={"profile_id": self.profile_id}, msg=str(qm["_id"]), action="store_validation_record_id",
                         html_id="")
         notify_frontend(data={"profile_id": self.profile_id}, msg=sample_data, action="make_table",
                         html_id="sample_table")
 
-    def make_upate_table(self):
+    def make_update_notifications(self, qm):
+        sample_data = self.data
+        updates = {}
+        for p in range(1, len(sample_data)):
+            s = map_to_dict(self.data.columns, self.data.iloc[p, :])
+            rack_tube = s.get("RACK_OR_PLATE_ID", "") + "/" + s["TUBE_OR_WELL_ID"]
+            if s["SYMBIONT"].upper() == "SYMBIONT":
+                # this requires different logic to discriminate between symbionts
+                return False
+            exsam = Sample().get_target_by_field("rack_tube", rack_tube)
+            assert len(exsam) == 1
+            exsam = exsam[0]
+            updates[rack_tube] = {}
+            for field in s.keys():
+                if s[field].strip() != exsam.get(field, "") and s[field].strip() != exsam["species_list"][0].get(field,
+                                                                                                                 ""):
+                    if field in lookup.DTOL_NO_COMPLIANCE_FIELDS[self.type.lower()]:
+                        updates[rack_tube][field] = {}
+                        if field in lookup.SPECIES_LIST_FIELDS:
+                            updates[rack_tube][field]["old_value"] = exsam["species_list"][0][field]
+                            updates[rack_tube][field]["new_value"] = s[field]
+                        else:
+                            updates[rack_tube][field]["old_value"] = exsam[field]
+                            updates[rack_tube][field]["new_value"] = s[field]
+                    else:
+                        msg = "Field " + field + " cannot be updated as it is part of the compliance process"
+                        notify_frontend(data={"profile_id": self.profile_id}, msg=msg, action="error",
+                                        html_id="sample_info")
+                        return False
+            # show upcoming updates here
+            msg = "<ul>"
+            for sample in updates:
+                msg += "<li>Updating sample <strong>" + sample + "</strong>: <ul>"
+                for field in updates[sample]:
+                    msg += "<li><strong> " + field + "</strong> from " + updates[sample][field]["old_value"] + " " \
+                                                                                                               "to <strong>" + \
+                           updates[sample][field]["new_value"] + "</strong></li>"
+                msg += "</li></ul>"
+            msg += "</ul>"
+            notify_frontend(data={"profile_id": self.profile_id}, msg=msg, action="warning",
+                            html_id="warning_info3")
+            notify_frontend(data={"profile_id": self.profile_id}, msg=sample_data.to_string(), action="make_update",
+                            html_id="sample_table")
