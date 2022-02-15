0203e78f99c78c175f43e2c6bed91899f7999ba8
shaw2thefloor <shaw2thefloor@gmail.com>
2022-02-15 12:57:24 +0000

    fixing infintite loop in validation

--- dal/copo_da.py
+++ dal/copo_da.py
@@ -327,12 +327,12 @@ class ValidationQueue(DAComponent):
         super(ValidationQueue, self).__init__(profile_id, "validationQueue")
 
     def get_queued_manifests(self):
-        m_list = self.get_collection_handle().aggregate([
-            {"$match": {"schema_validation_status": "pending", "taxon_validation_status": "pending"}},
-            {"$addFields": {"schema_validation_status": "processing", "taxon_validation_status": "processing"}}
-        ]
-        )
-        return m_list
+        m_list = self.get_collection_handle().find({"schema_validation_status": "pending", "taxon_validation_status": "pending"})
+        out = list(m_list)
+        for el in out:
+            self.get_collection_handle().update_one({"_id": el["_id"]}, {"$set": {"schema_validation_status": "processing", "taxon_validation_status":
+                "processing"}})
+        return out
 
     def update_manifest_data(self, record_id, manifest_data):
         self.get_collection_handle().update_one({"_id": ObjectId(record_id)}, {"$set": {"manifest_data": manifest_data}})
