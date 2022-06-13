b51e6480c5ad9647363f99131a1e375a4b80bac1
Sherida101 <aaliyahsp@hotmail.com>
2022-06-13 15:49:16 +0100

    Added "as_asgi()" in the web/apps/web_copo/routing.py file to fix the TypeError: __call__() missing 1 required positional argument: 'send' for Django channels

--- web/apps/web_copo/routing.py
+++ web/apps/web_copo/routing.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import consumers
 
+# as_asgi() is added to prevent the TypeError: __call__() missing 1 required positional argument: 'send'
+
 websocket_urlpatterns = [
-    path('ws/submission_status/<str:profile_id>/', consumers.SubmissionConsumer),
-    path('ws/sample_status/<str:profile_id>', consumers.SampleConsumer),
-    path('ws/dtol_status', consumers.DtolConsumer)
+    path('ws/submission_status/<str:profile_id>/', consumers.SubmissionConsumer.as_asgi()),
+    path('ws/sample_status/<str:profile_id>', consumers.SampleConsumer.as_asgi()),
+    path('ws/dtol_status', consumers.DtolConsumer.as_asgi())
 ]
