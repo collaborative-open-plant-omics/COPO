9effab93a8e0b23fcb989e859b3c23ad67978b47
Sherida101 <aaliyahsp@hotmail.com>
2022-07-14 17:33:45 +0100

    Moved some js code to the landing.js file from the manifests webpage; Added Ajax code to populate the dropdown list fir the common values for the field names step in the manifest wizard; Removed external font awesome CSS to use the font awesome CSS that already exists in the project and revised the code to suit

--- static/assets/js/landing.js
+++ static/assets/js/landing.js
@@ -1,10 +1,32 @@
 $(document).ready(function () {
+    // Manifest webpage
+    // Trigger manifest wizard modal
+    $(document).on("click", "#show_manifest_wzd_button", function (e) {
+        $("#modal-wizard").modal("show");
+    });
+    // Number of organisms/samples/rows step
+    var select = "";
+    for (let i = 1; i <= 100; i++) {
+        select += "<option val=" + i + ">" + i + "</option>";
+    }
+    $("#numberOfSamples").html(select);
 
+    // Generate a dropdown menu for common field values
+    $("#commonvalue").selectpicker();
+    // Hides the modal and clear the data within the modal
+    // after the "ok" button is clicked in the popup dialog
+    $(document).on("click", ".okbtn", function (e) {
+        $('#manifestType').val([]);
+        $('#numberOfSamples').val([1]);
+        $('#commonvalue').val([]);
+        $("#modal-wizard").modal("hide");
+    });
 
+    // Other code
     $(document).on("click", ".card", function () {
         window.location = "/copo/stats#"
     })
-    
+
     $('.ui.dropdown').dropdown();
     var image = getRandomInt(images.length)
     $('body').css("background-image", "url(" + images[image] + ")")
@@ -26,7 +48,7 @@ $(document).ready(function () {
     //     console.log(data)
     // })
 
-})
+});
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
