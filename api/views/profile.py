d75509995c34d2ecbf47368345d2d4a7a5a1c3e4
shaw2thefloor <shaw2thefloor@gmail.com>
2022-08-22 17:31:28 +0100

    fixing various swagger parameter issues, programmatic creation of profiles now working

--- api/views/profile.py
+++ api/views/profile.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated, AllowAny
+import json
+from bson import json_util
+from dal.copo_da import Profile
 
 
 class APICreateProfile(APIView):
     def post(self, request):
-        x = {"text": "abc"}
-        return Response(x)
+        uid = request.user.id
+        p_dict = {"title": request.POST["title"], "description": request.POST["description"], "profile_type": request.POST["profile_type"],
+                  "user_id": uid}
+        p = Profile().save_record({}, **p_dict)
+        return HttpResponse(json_util.dumps(p))
