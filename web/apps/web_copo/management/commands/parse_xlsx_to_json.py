4dd7ac3c5c5366d2f1f2d25c7e36162519884262
Sherida101 <aaliyahsp@hotmail.com>
2022-10-19 15:50:39 +0100

    Made progress on parsing the spreadsheet to .json file

--- web/apps/web_copo/management/commands/parse_xlsx_to_json.py
+++ web/apps/web_copo/management/commands/parse_xlsx_to_json.py
@@ -1,53 +1,15 @@
+from bson import json_util
 from dal import cursor_to_list
-from dal.copo_da import Sample
+from dal.copo_da import Sample, DAComponent
 from django.conf import settings as settings
 from django.core.management import BaseCommand
 from xlrd import open_workbook, XLRDError
-import dal.copo_da as da
-import numpy as np
-import pandas as pd
 import ast
 import dal.copo_da as da
 import importlib
+import json
 import pandas as pd
 import re
-import xml.etree.cElementTree as ET
-
-# samplesindb = da.Sample().get_by_biosample_ids(list(d_updates.keys()))
-#
-# # if there's source update it
-# if sample.get("sampleDerivedFrom", ""):
-#     source_biosample = sample.get("sampleDerivedFrom")
-# elif sample.get("sampleSameAs", ""):
-#     source_biosample = sample.get("sampleSameAs")
-# else:
-#     source_biosample = ""
-# if source_biosample:
-#     sourceindb = da.Source().get_by_field("biosampleAccession", source_biosample)
-#     assert len(sourceindb) == 1
-#     for field in d_updates[sample['biosampleAccession']]:
-#         # only update in source fields that are there -ENA submittable- and not organism part
-#         # unique handling of COLLECTION_LOCATION
-#         if field == "COLLECTION_LOCATION":
-#             value = d_updates[sample['biosampleAccession']][field]
-#             da.Source().record_manual_update(field, oldvalue, value, sourceindb[0]['_id'])
-#         elif field != "ORGANISM_PART" and DTOL_ENA_MAPPINGS.get(field, ""):
-#             value = d_updates[sample['biosampleAccession']][field]
-#             da.Source().record_manual_update(field, oldvalue, value, sourceindb[0]['_id'])
-#         da.Source().add_field(field, value, sourceindb[0]['_id'])
-
-
-# manifest_id = options["manifest_id"]
-# fromdb = da.handle_dict["sample"].count({"manifest_id": manifest_id, "status": "processing"})
-# print("samples stuck: " + str(fromdb))
-# fromdb = da.handle_dict["sample"].update_many({"manifest_id": manifest_id, "status": "processing"},
-#                                               {"$set": {"status": "pending"}})
-# print("samples unstuck: " + str(fromdb.modified_count))
-# print("done")
-
-schema_version_path_dtol_lookups = f'web.apps.web_copo.schema_versions.{settings.CURRENT_SCHEMA_VERSION}.lookup.dtol_lookups'
-dtol_lookups_data = importlib.import_module(schema_version_path_dtol_lookups)
-DTOL_ENA_MAPPINGS = dtol_lookups_data.DTOL_ENA_MAPPINGS
 
 '''
 Extract "SPECIMEN_ID" value from a Excel spreadsheet then, retrieve the sample metadata 
@@ -57,6 +19,11 @@ Output the result in a .json file
 
 e.g. python3 manage.py parse_xlsx_to_json "/usr/users/EI_ga012/providen/Downloads/nhmdump.xlsx"
 '''
+
+schema_version_path_dtol_lookups = f'web.apps.web_copo.schema_versions.{settings.CURRENT_SCHEMA_VERSION}.lookup.dtol_lookups'
+dtol_lookups_data = importlib.import_module(schema_version_path_dtol_lookups)
+DTOL_ENA_MAPPINGS = dtol_lookups_data.DTOL_ENA_MAPPINGS
+TOL_PROFILE_TYPES = dtol_lookups_data.TOL_PROFILE_TYPES
 
 
 class Command(BaseCommand):
@@ -73,8 +40,7 @@ class Command(BaseCommand):
     # A command must define handle()
     def handle(self, *args, **options):
         file_path_dict = ast.literal_eval(str(options))  # '/usr/users/EI_ga012/providen/Downloads/nhmdump.xlsx'
-        excel_file_path = file_path_dict.get(
-            "xlsx")  # '/usr/users/EI_ga012/providen/Downloads/DToL_sample_output_220930.xml'
+        excel_file_path = file_path_dict.get("xlsx")
         try:
             open_workbook(excel_file_path)
             df = pd.read_excel(excel_file_path)  # Convert excel file to  a Pandas dataframe
@@ -100,37 +66,87 @@ class Command(BaseCommand):
                     specimen_id = "".join(specimen_id.split(pattern_with_Emu_only))
                     specimen_id_list.append(specimen_id)
 
-            print("Specimen IDs from excel file: ", specimen_id_list)
-            print("Number of Specimen IDs in the list: ", len(specimen_id_list))
             assert 2138 == len(specimen_id_list)  # Number of SPECIMEN_IDs is equal to 2138
-            # biosample_and_specimen_id_list =
-            sample_list = []
+
             specimen_ids_not_in_db = []
             samples_only_in_db = []
-            sources_only_in_db_list = []
+            sources_only_in_db = []
             samples_and_sources_in_db = []
+            specimen_id_list = ["MBA-190930-001A", "MBA-190930-001B"]
+
             for specimen in specimen_id_list:
-                sample_in_db = cursor_to_list(Sample().get_collection_handle().find({"SPECIMEN_ID": specimen}))
-                source_in_db = cursor_to_list(da.Source().get_collection_handle().find({"SPECIMEN_ID": specimen}))
-                #  source = sample == [] ? cursor_to_list(Source().get_collection_handle().find({"SPECIMEN_ID": specimen})) : sample
-                sample_list.append(cursor_to_list(Sample().get_collection_handle().find(
-                    {"SPECIMEN_ID": specimen})))
+                sample_in_db = cursor_to_list(Sample().get_sample_by_specimen_id(specimen))
+                source_in_db = cursor_to_list(da.Source().get_by_specimen(specimen))
 
                 if sample_in_db == [] and source_in_db == []:
                     specimen_ids_not_in_db.append(specimen)
                 elif sample_in_db != [] and source_in_db == []:
                     samples_only_in_db.append(sample_in_db)
                 elif sample_in_db == [] and source_in_db != []:
-                    sources_only_in_db_list.append(source_in_db)
+                    sources_only_in_db.append(source_in_db)
                 else:
                     # sample_in_db != [] and source_in_db != []
                     samples_and_sources_in_db.append(sample_in_db)
                     samples_and_sources_in_db.append(source_in_db)
-            print("List of samples in db: ", sample_list)
+
             print("List of specimens not found in db: ", specimen_ids_not_in_db)
             print("List of samples found in db: ", samples_only_in_db)
-            print("List of sources found in db: ", sources_only_in_db_list)
+            print("List of sources found in db: ", sources_only_in_db)
             print("List of samples and sources found in db: ", samples_and_sources_in_db)
+
+            # Get the database field name based on the 'ena' key in the "DTOL_ENA_MAPPINGS" dictionary
+            #             fields = [field for field, field_value in DTOL_ENA_MAPPINGS.items() if
+            #                       field_value['ena'] == field.text]
+            #             field_values.append(value.text)
+
+            # Convert list of "SPECIMEN_ID" not present in COPO to json
+            samples_only_df = pd.DataFrame(data=[specimen_ids_not_in_db], columns=["SPECIMEN_ID"])
+            samples_only_df.to_json('specimen_ids_not_present_in_copo.json')
+
+            # Convert data in db to json
+            # Samples only
+            if samples_only_in_db != [] and sources_only_in_db == []:
+                samples_only_in_db = json.loads(json_util.dumps(samples_only_in_db))
+                fields = list(samples_only_in_db[0][0].keys())
+                field_values = list(samples_only_in_db[0][0].values())
+                print("Keys: ", fields)
+                print("\n\n")
+                print("Values: ", field_values)
+
+                field_values = [list(item.values())[0] if type(item) is dict else item for item in field_values]
+                print("Values 2: ", field_values)
+                samples_only_df = pd.DataFrame(data=[field_values], columns=fields)
+                samples_only_df.to_json('copo_samples_only.json')
+            elif sources_only_in_db != [] and samples_only_in_db == []:
+                # Sources only
+                sources_only_in_db = json.loads(json_util.dumps(sources_only_in_db))
+                fields = list(sources_only_in_db[0][0].keys())
+                field_values = list(sources_only_in_db[0][0].values())
+                print("Keys: ", fields)
+                print("\n\n")
+                print("Values: ", field_values)
+
+                field_values = [list(item.values())[0] if type(item) is dict else item for item in field_values]
+                print("Values 2: ", field_values)
+                sources_only_df = pd.DataFrame(data=[field_values], columns=fields)
+                sources_only_df.to_json('copo_sources_only.json')
+            else:
+                # Samples and sources
+                # sample_in_db != [] and source_in_db != []
+                samples_and_sources_in_db = json.loads(json_util.dumps(samples_and_sources_in_db))
+                fields = list(samples_and_sources_in_db[0][0].keys())
+                field_values = list(samples_and_sources_in_db[0][0].values())
+                print("Keys: ", fields)
+                # print("\n\n")
+                print("Values: ", field_values)
+                print(field_values[0])
+                # print("\n\n")
+                # print([element for index, element in enumerate(field_values) if "$oid" in field_values])
+                # columns = [sample. for sample in samples_only_in_db[0]]
+                field_values = [list(item.values())[0] if type(item) is dict else item for item in field_values]
+                print("Values 2: ", field_values)
+                samples_and_sources_only_df = pd.DataFrame(data=[field_values], columns=fields)
+                samples_and_sources_only_df.to_json('copo_samples_and_sources.json')
 
         except XLRDError as error:
             # File format is unsupported or file is corrupt
