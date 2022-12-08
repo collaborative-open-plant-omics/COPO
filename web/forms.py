119b8888fdb2c8b8a5dd047fe4c9a766a5208ff9
aliceminotto <i.m.e@hotmail.it>
2022-12-08 15:21:15 +0000

    better looking form with choice and optional fields fields

--- web/forms.py
+++ web/forms.py
@@ -1,16 +1,27 @@
 from django import forms
 
-class AssemblyForm(forms.Form):
-    #fields from ENA assembly documentation
-    study = forms.CharField(label = "STUDY") #STUDY: Study accession - mandatory
-    sample = forms.CharField(label = "SAMPLE") #SAMPLE: Sample accession - mandatory
-    assemblyname = forms.CharField(label = "ASSEMBLYNAME") #ASSEMBLYNAME: Unique assembly name, user-provided - mandatory
-    assembly_type = forms.ChoiceField(label = "ASSEMBLY_TYPE") #ASSEMBLY_TYPE: ‘clone or isolate’ - mandatory
-    coverage = forms.CharField(label = "COVERAGE") #COVERAGE: The estimated depth of sequencing coverage - mandatory
-    program = forms.CharField(label = "PROGRAM") #PROGRAM: The assembly program - mandatory
-    platform = forms.CharField(label = "PLATFORM") #PLATFORM: The sequencing platform, or comma-separated list of platforms - mandatory
-    mingaplength = forms.CharField(label = "MINGAPLENGTH", required=False) #MINGAPLENGTH: Minimum length of consecutive Ns to be considered a gap - optional
-    molecoletype = forms.ChoiceField(label = "MOLECULETYPE", required=False) #MOLECULETYPE: ‘genomic DNA’, ‘genomic RNA’ or ‘viral cRNA’ - optional
-    description = forms.CharField(widget=forms.Textarea, required=False) #DESCRIPTION: Free text description of the genome assembly - optional
-    run_ref = forms.CharField(label = "RUN_REF", required=False) #RUN_REF: Comma separated list of run accession(s) - optional
 
+class AssemblyForm(forms.Form):
+    # fields from ENA assembly documentation
+    study = forms.CharField(label="STUDY", widget=forms.TextInput(attrs={'placeholder': 'Study accession'}))
+    sample = forms.CharField(label="SAMPLE", widget=forms.TextInput(attrs={'placeholder': 'Sample accession'}))
+    assemblyname = forms.CharField(label="ASSEMBLYNAME", widget=forms.TextInput(attrs={'placeholder': 'Unique '
+                                                                                                      'assembly name,'
+                                                                                                      ' user-provided'}))
+    assembly_type = forms.ChoiceField(label="ASSEMBLY_TYPE", choices=[('clone', 'clone'), ('isolate', 'isolate')])
+    coverage = forms.CharField(label="COVERAGE", widget=forms.TextInput(attrs={'placeholder': 'The estimated depth of '
+                                                                                              'sequencing coverage'}))
+    program = forms.CharField(label="PROGRAM", widget=forms.TextInput(attrs={'placeholder': 'The assembly program'}))
+    platform = forms.CharField(label="PLATFORM", widget=forms.TextInput(attrs={'placeholder': 'The sequencing '
+                                                                                              'platform, '
+                                                                                              'or comma-separated '
+                                                                                              'list of platforms'}))
+    mingaplength = forms.CharField(label="MINGAPLENGTH", required=False, widget=forms.TextInput(attrs={'placeholder': 'Minimum length of consecutive Ns to be considered a gap'}))
+    molecoletype = forms.ChoiceField(label="MOLECULETYPE", required=False,
+                                     choices=[('',''),('genomic DNA', 'genomic DNA'), ('genomic RNA', 'genomic RNA'),
+                                              ("viral cRNA", "viral cRNA")])
+    description = forms.CharField( required=False,
+                                  widget=forms.Textarea(attrs={'placeholder': 'Free text description of the genome '
+                                                                              'assembly'}))
+    run_ref = forms.CharField(label="RUN_REF", required=False, widget=forms.TextInput(attrs={'placeholder': 'Comma '
+                                                                                                            'separated list of run accession(s)'}))
