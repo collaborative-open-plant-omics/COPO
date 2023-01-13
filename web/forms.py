e2558c09b55c4e496f9ad6a6bad6fac477ecca2b
shaw2thefloor <shaw2thefloor@gmail.com>
2023-01-13 16:54:17 +0000

    more minor fixes...removing fields which can cause error and file inputs which are not supported

--- web/forms.py
+++ web/forms.py
@@ -19,15 +19,15 @@ class AssemblyForm(forms.Form):
             self.fields['sample'].required = False
             self.fields['sample'].hidden = True
             pass
-            #todo this bit does not really work, a drop down menu shows up no matter what (altough empty and optional)
-            #so it works but it's an aesthetic problem
-            #self.fields['sample'].hidden = True
-            #self.fields['sample'].label = ''
-            #self.fields['sample'].required = False
+            # todo this bit does not really work, a drop down menu shows up no matter what (altough empty and optional)
+            # so it works but it's an aesthetic problem
+            # self.fields['sample'].hidden = True
+            # self.fields['sample'].label = ''
+            # self.fields['sample'].required = False
 
     # fields from ENA assembly documentation
     study = forms.CharField(label="STUDY",
-                                           widget=forms.TextInput(attrs={'placeholder': 'Study accession'}))
+                            widget=forms.TextInput(attrs={'placeholder': 'Study accession'}))
     sample = forms.ChoiceField(label="SAMPLE")
     sample_text = forms.CharField(label="SAMPLE", widget=forms.TextInput(attrs={'placeholder': 'Sample accession'}), required=False)
     assemblyname = forms.CharField(label="ASSEMBLYNAME", widget=forms.TextInput(attrs={'placeholder': 'Unique '
@@ -46,17 +46,17 @@ class AssemblyForm(forms.Form):
                                           attrs={'placeholder': 'Minimum length of consecutive Ns to '
                                                                 'be considered a gap'}))
     moleculetype = forms.ChoiceField(label="MOLECULETYPE", required=False,
-                                     choices=[('', ''), ('genomic DNA', 'genomic DNA'), ('genomic RNA', 'genomic RNA'),
+                                     choices=[('genomic DNA', 'genomic DNA'), ('genomic RNA', 'genomic RNA'),
                                               ("viral cRNA", "viral cRNA")])
     description = forms.CharField(label="DESCRIPTION", required=False,
                                   widget=forms.Textarea(attrs={'placeholder': 'Free text description of the genome '
                                                                               'assembly'}))
-    run_ref = forms.CharField(label="RUN_REF", required=False, widget=forms.TextInput(
-        attrs={'placeholder': 'Comma separated list of run accession(s)'}))
-    fasta = forms.FileField(label="FASTA", required=False, widget=forms.FileInput(
-        attrs={'accept': '.fasta.gz, .fas.gz, .fsa.gz, fna.gz, .fa.gz, .fasta.bz2, .fas.bz2, .fsa.bz2, .fna.bz2, '
-                         '.fa.bz2'}))
-    flatfile = forms.FileField(label="FLATFILE", required=False)
-    agp = forms.FileField(label="AGP", required=False)
-    chromosome_list = forms.FileField(label="CHROMOSOME_LIST", required=False)
-    unlocalised_list = forms.FileField(label="UNLOCALISED_LIST", required=False)
+    # run_ref = forms.CharField(label="RUN_REF", required=False, widget=forms.TextInput(
+    #    attrs={'placeholder': 'Comma separated list of run accession(s)'}))
+    # fasta = forms.FileField(label="FASTA", required=False, widget=forms.FileInput(
+    #    attrs={'accept': '.fasta.gz, .fas.gz, .fsa.gz, fna.gz, .fa.gz, .fasta.bz2, .fas.bz2, .fsa.bz2, .fna.bz2, '
+    #                     '.fa.bz2'}))
+    # flatfile = forms.FileField(label="FLATFILE", required=False)
+    # agp = forms.FileField(label="AGP", required=False)
+    # chromosome_list = forms.FileField(label="CHROMOSOME_LIST", required=False)
+    # unlocalised_list = forms.FileField(label="UNLOCALISED_LIST", required=False)
