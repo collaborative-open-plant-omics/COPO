0c6e7a5c9f6337477c50b1b6b32efeac7ee31a79
shaw2thefloor <shaw2thefloor@gmail.com>
2023-01-17 16:22:48 +0000

    adding form control in which was commented out in error

--- web/forms.py
+++ web/forms.py
@@ -53,9 +53,9 @@ class AssemblyForm(forms.Form):
                                                                               'assembly'}))
     # run_ref = forms.CharField(label="RUN_REF", required=False, widget=forms.TextInput(
     #    attrs={'placeholder': 'Comma separated list of run accession(s)'}))
-    # fasta = forms.FileField(label="FASTA", required=False, widget=forms.FileInput(
-    #    attrs={'accept': '.fasta.gz, .fas.gz, .fsa.gz, fna.gz, .fa.gz, .fasta.bz2, .fas.bz2, .fsa.bz2, .fna.bz2, '
-    #                     '.fa.bz2'}))
+    fasta = forms.FileField(label="FASTA", required=False, widget=forms.FileInput(
+        attrs={'accept': '.fasta.gz, .fas.gz, .fsa.gz, fna.gz, .fa.gz, .fasta.bz2, .fas.bz2, .fsa.bz2, .fna.bz2, '
+                         '.fa.bz2'}))
     # flatfile = forms.FileField(label="FLATFILE", required=False)
     # agp = forms.FileField(label="AGP", required=False)
     # chromosome_list = forms.FileField(label="CHROMOSOME_LIST", required=False)
