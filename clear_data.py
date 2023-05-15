0dedbfe2107cfeeaf1df0194865a2efc97b29bf4
debbyku <debbyku.2008@gmail.com>
2023-05-15 11:26:38 +0000

    initial commit for sequence annotation

---
+++ clear_data.py
@@ -0,0 +1,22 @@
+import pymongo
+from pymongo import ReturnDocument
+import pymongo.errors as pymongo_errors
+import urllib.parse
+
+username = urllib.parse.quote_plus("copo_user")
+password = urllib.parse.quote_plus("password")
+myclient = pymongo.MongoClient("mongodb://%s:%s@copo_mongo:27017/" % (username, password))
+mydb = myclient["copo_mongo"]
+
+def drop_collection(name):
+    print(f"Drop {name}")
+    mycol = mydb[name]
+    mycol.drop()
+    print(f"{name} dropped")
+
+drop_collection("SampleCollection")
+drop_collection("SourceCollection")
+drop_collection("SubmissionCollection")
+drop_collection("ValidationQueueCollection")
+drop_collection("Profiles")
+drop_collection("DataFileCollection")
