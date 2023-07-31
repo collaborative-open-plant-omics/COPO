c6fb686708d3bae4cc21685583b87ceb91ca391f
Sherida101 <aaliyahsp@hotmail.com>
2023-07-31 09:51:46 +0100

    Omitted ORCID PostgreSQL query; Added 'social_accounts' setup script

--- setup_scripts/3_db_setup.sh
+++ setup_scripts/3_db_setup.sh
@@ -23,11 +23,12 @@ python manage.py migrate
 python manage.py setup_groups
 python manage.py setup_schemas
 python manage.py createcachetable
+python manage.py social_accounts
 
 # setup allauth social accounts....N.B. you should have environmental variables set for $ORCID_CLIENT_ID and $ORCID_SECRET
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c 'DELETE FROM socialaccount_socialapp_sites'
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c 'DELETE FROM django_site'
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c 'DELETE FROM socialaccount_socialapp'
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c "INSERT INTO django_site (id, domain, name) VALUES (1, 'www.copo-project.org', 'www.copo-project.org')"
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c "INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (1, 'orcid', 'Orcid', '$ORCID_CLIENT_ID', '$ORCID_SECRET', '')"
-export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U 'copo_user' -d 'copo' -c 'INSERT INTO socialaccount_socialapp_sites (id, socialapp_id, site_id) VALUES (1, 1, 1)'
+export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c 'DELETE FROM socialaccount_socialapp_sites'
+export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c 'DELETE FROM django_site'
+export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c 'DELETE FROM socialaccount_socialapp'
+export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c "INSERT INTO django_site (id, domain, name) VALUES (1, 'www.copo-project.org', 'www.copo-project.org')"
+# export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c "INSERT INTO socialaccount_socialapp (id, provider, name, client_id, secret, key) VALUES (1, 'orcid', 'Orcid', '$ORCID_CLIENT_ID', '$ORCID_SECRET', '')"
+export PGPASSWORD=$POSTGRES_PASSWORD; psql -h 'localhost' -U  $POSTGRES_USER -d 'copo' -c 'INSERT INTO socialaccount_socialapp_sites (id, socialapp_id, site_id) VALUES (1, 1, 1)'
