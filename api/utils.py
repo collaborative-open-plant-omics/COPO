996ed4bb7267e7ea5ae11f3ea1d20b8ca2d515fb
debbyku <debbyku.2008Wgmail.com>
2023-09-08 16:04:52 +0000

    initial commit for api rocrate output

--- api/utils.py
+++ api/utils.py
@@ -88,15 +88,18 @@ def finish_request(template=None, error=None, num_found=None, return_http_respon
     else: 
         return output
 
-def generate_rocrate_response(data):
+def generate_rocrate_response_old(data):
     result_list = ["Not Implemented"]
     return result_list
 
 
-def generate_rocrate_response_experiment(data):
+def generate_rocrate_response(data):
     result_list = []
     manifest_map = dict()
     for samples in data:
+        if type(samples) != dict or  "manifest_id" not in samples:
+            result_list = ["Not Implemented"]
+            return result_list
         manifest_id = samples.get("manifest_id","")
         if manifest_id:
             if manifest_id not in manifest_map:
@@ -104,48 +107,98 @@ def generate_rocrate_response_experiment(data):
             manifest_map[manifest_id].append(samples)
 
     for key, samples in manifest_map.items():
-        rocrate_list = list()
-
-        context = dict()
-        context["@context"] = [
+        rocrate_json = {}
+        
+        context = [
                     "https://w3id.org/ro/crate/1.1/context",
                     "https://w3id.org/ro/terms/sample",
                     "https://w3id.org/ro/terms/copo"
-                ],
-
-        item_list = list()
+                ]
+        rocrate_json["@context"] = context
+        graph_list = list()
         #create creativework
-        creativeWork = dict(id="ro-crate-metadata.json", type="CreativeWork")
-        conformsTo = {"@id": "https://w3id.org/ro/crate/1.1", "about": {"@id":f"https://copo-project.org/api/manifest/{key}" }}
-        creativeWork.update(conformsTo)
-        item_list.append(creativeWork)
+        creativeWork =  {"@id":"ro-crate-metadata.json", "@type" : "CreativeWork"}
+        creativeWork["conformsTo"] = {"@id": "https://w3id.org/ro/crate/1.1"}
+        creativeWork["about"] = {"@id":f"https://copo-project.org/api/manifest/{key}" }
+        graph_list.append(creativeWork)
 
         df = pd.DataFrame(samples)
         dateCreated = df["time_created"].min()
         dateModifed = df["time_updated"].max()
-        author = df["created_by"].unique()
-        item = {"@id":f"https://copo-project.org/api/manifest/{key}", "@type":"Dataset", "dateCreated": dateCreated, "datedModified":dateModifed} 
-        item["author"] = author
-        item["contributor"] = df["updated_by"].unique()
-        item["hasPart"] = [ { "@id": f"https://copo-project.org/api/sample/copo_id/{x['copo_id']}"} for x in samples ] 
-        item["about"] = [ { "@id": f"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id={x}"} for x in df["TAXON_ID"].unique() ] 
-        item_list.append(item)       
+
+        #updatedby = df["updated_by"].unique()
+        #author = df["created_by"].unique()
+        collectedby = df["COLLECTED_BY"].unique()
+        coordinator =  df["SAMPLE_COORDINATOR"].unique() if "SAMPLE_COORDINATOR" in df.columns else []
+        perservedby = df["PERSERVED_BY"].unique() if "PERSERVED_BY" in df.columns else []
+        identifiedby = df["IDENTIFIED_BY"].unique() if "IDENTIFIED_BY" in df.columns else []
+
+        rocrate_person = [] 
+        rocrate_person.extend(generate_rocrate_person_object(df, collectedby, "COLLECTED_BY", "COLLECTOR"))
+        rocrate_person.extend(generate_rocrate_person_object(df, coordinator, "SAMPLE_COORDINATOR", "SAMPLE_COORDINATOR"))
+        rocrate_person.extend(generate_rocrate_person_object(df, perservedby, "PERSERVED_BY", "PERSERVER"))
+        rocrate_person.extend(generate_rocrate_person_object(df, identifiedby, "IDENTIFIED_BY", "IDENTIFIER"))
+
+       
+        manifest_item = {"@id":f"https://copo-project.org/api/manifest/{key}", "@type":"Dataset", "dateCreated": dateCreated, "datedModified":dateModifed} 
+        manifest_item["contributor"] = []
+        manifest_item["contributor"].extend( {"@id" :  p["@id"]} for p in rocrate_person  )
+        
+        manifest_item["hasPart"] = [ { "@id": f"https://copo-project.org/api/sample/copo_id/{x['copo_id']}"} for x in samples ] 
+        manifest_item["taxonomicRange"] = [ { "@id": f"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id={x}"} for x in df["TAXON_ID"].unique() ] 
+        graph_list.append(manifest_item)       
+        graph_list.extend(rocrate_person)
 
 
         for x in df["TAXON_ID"].unique():
              item = {"@id": f"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id={x}"}
              item["@type"] = "TAXON"
-             item["name"] = df[df["TAXON_ID"]== x ]["SCIENTIFIC_NAME"][0]
-             item["parentTaxon"] = {"@id": f"https://copo-project.org/api/sample_field/ORDER_OR_GROUP/{df[df['TAXON_ID']== x ]['ORDER_OR_GROUP'][0]}"}    
-             item_list.append(item)
+             item["name"] = df[df["TAXON_ID"]== x ]["SCIENTIFIC_NAME"].unique()[0] if "SCIENTIFIC_NAME" in df.columns else ""
+             item["parentTaxon"] = {"@id": f"https://copo-project.org/api/sample_field/ORDER_OR_GROUP/{df[df['TAXON_ID']== x ]['ORDER_OR_GROUP'].unique()[0]}"}    
+             graph_list.append(item)
 
+        for x in samples:
+            sample_item = { "@id": f"https://copo-project.org/api/sample/copo_id/{x['copo_id']}", "@type":"Sample"  } 
+            sample_item.update(x)
+            graph_list.append(sample_item) 
 
-        context["@graph"] = item_list
-        rocrate_list.append(context)
-
-        result_list.append(rocrate_list)
+        rocrate_json["@graph"] = graph_list
+        result_list.append(rocrate_json)
     return result_list
 
+
+def generate_rocrate_person_object(df, personList, person_field_name, prefix) :
+    items = []
+    for people in personList:
+        person_affiliations =  df[df[person_field_name]== people ][f"{prefix}_AFFILIATION"].unique()[0]  if f"{prefix}_AFFILIATION" in df.columns else str()   
+        orcid_ids =  df[df[person_field_name]== people ][f"{prefix}_ORCID_ID"].unique()[0] if f"{prefix}_ORCID_ID" in df.columns else str()
+        
+        person_list = people.split("|")
+        person_affiliation_list = person_affiliations.split("|")
+        orcid_id_list = orcid_ids.split("|")
+        affiliation = ""
+        
+        for index,  person in  enumerate(person_list):
+            item = {}
+
+            if orcid_ids and len(orcid_id_list) > index:
+                item["@id"] = "http://orcid.org/" + orcid_id_list[index].strip()
+                item["name"] = person.strip()
+            else:
+                item["@id"] = person.strip()
+
+            item["@type"] = "Person"
+            item["role"] = person_field_name
+
+            if person_affiliation_list :
+                if len(person_affiliation_list) > index:
+                    item["affiliation"] = person_affiliation_list[index].strip()
+                    affiliation = item["affiliation"]
+                elif affiliation:
+                    item["affiliation"] = affiliation
+            items.append(item)    
+
+    return items
 
 '''
     response = """
