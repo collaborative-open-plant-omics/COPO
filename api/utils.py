7ea841b04ff9458b5e23518ee42eaf9404fc9d84
Sherida101 <aaliyahsp@hotmail.com>
2023-09-11 12:33:28 +0000

    Added "Place" object type; "custodian" attribute

--- api/utils.py
+++ api/utils.py
@@ -8,6 +8,7 @@ from django_tools.middlewares import ThreadLocal
 
 from web.apps.web_copo.lookup.lookup import API_RETURN_TEMPLATES
 import uuid
+import shortuuid
 
 def get_return_template(type):
     """
@@ -93,44 +94,64 @@ def generate_rocrate_response_old(data):
     return result_list
 
 
-def generate_rocrate_response(data):
-    result_list = []
-    manifest_map = dict()
-    for samples in data:
-        if type(samples) != dict or  "manifest_id" not in samples:
-            result_list = ["Not Implemented"]
-            return result_list
-        manifest_id = samples.get("manifest_id","")
-        if manifest_id:
-            if manifest_id not in manifest_map:
-                manifest_map[manifest_id] = []
-            manifest_map[manifest_id].append(samples)
+def generate_rocrate_response(samples):
+    # result_list = []
+    # manifest_map = dict()
+    # for samples in data:
+    #     if type(samples) != dict or  "manifest_id" not in samples:
+    #         result_list = ["Not Implemented"]
+    #         return result_list
+    #     manifest_id = samples.get("manifest_id","")
+    #     if manifest_id:
+    #         if manifest_id not in manifest_map:
+    #             manifest_map[manifest_id] = []
+    #         manifest_map[manifest_id].append(samples)
 
-    if len(manifest_map.keys()) > 1:
-        result_list = ["Not Implemented"]
-        return result_list
+    # if len(manifest_map.keys()) > 1:
+    #     result_list = ["Not Implemented"]
+    #     return result_list
 
+    rocrate_json = {}
+    manifest_id = samples[0].get("manifest_id","")
 
-    for key, samples in manifest_map.items():
-        rocrate_json = {}
-        
+    if not manifest_id:
+        rocrate_json["error"] = "Not Implemented"
+    else:
+        # @type: context   
+        # for key, samples in manifest_map.items():
+            # rocrate_json = {}
         context = [
                     "https://w3id.org/ro/crate/1.1/context",
                     "https://w3id.org/ro/terms/sample",
                     "https://w3id.org/ro/terms/copo"
                 ]
+
+        info_dict =  {
+            "dwc":  "http://rs.tdwg.org/dwc/terms/",
+            "dwciri": "http://rs.tdwg.org/dwc/iri/",
+            "BioSample": "https://bioschemas.org/BioSample",
+            "collector": "https://bioschemas.org/BioSample#collector",
+            "custodian": "https://bioschemas.org/BioSample#custodian",
+            "isControl": "https://bioschemas.org/BioSample#isControl",
+            "samplingAge": "https://bioschemas.org/BioSample#samplingAge",
+            "taxonomicRange": "http://schema.org/taxonomicRange"
+        }
+
+        context.append(info_dict)
         rocrate_json["@context"] = context
         graph_list = list()
-        #create creativework
+
+        # @type: CreativeWork
         creativeWork =  {"@id":"ro-crate-metadata.json", "@type" : "CreativeWork"}
         creativeWork["conformsTo"] = {"@id": "https://w3id.org/ro/crate/1.1"}
-        creativeWork["about"] = {"@id":f"https://copo-project.org/api/manifest/{key}" }
+        creativeWork["about"] = {"@id":f"https://copo-project.org/api/manifest/{manifest_id}" } # {"@id":f"https://copo-project.org/api/manifest/{key}" }
         graph_list.append(creativeWork)
 
         df = pd.DataFrame(samples)
         dateCreated = df["time_created"].min()
         dateModifed = df["time_updated"].max()
 
+        # @type: Person
         #updatedby = df["updated_by"].unique()
         #author = df["created_by"].unique()
         collectedby = df["COLLECTED_BY"].unique()
@@ -143,48 +164,94 @@ def generate_rocrate_response(data):
         rocrate_person = generate_rocrate_person_object(df, coordinator, "SAMPLE_COORDINATOR", "SAMPLE_COORDINATOR",rocrate_person)
         rocrate_person = generate_rocrate_person_object(df, perservedby, "PERSERVED_BY", "PERSERVER",rocrate_person)
         rocrate_person = generate_rocrate_person_object(df, identifiedby, "IDENTIFIED_BY", "IDENTIFIER",rocrate_person)
-       
-        manifest_item = {"@id":f"https://copo-project.org/api/manifest/{key}", "@type":"Dataset", "dateCreated": dateCreated, "datedModified":dateModifed} 
+    
+        # @type: Dataset
+        # f"https://copo-project.org/api/manifest/{key}
+        manifest_item = {"@id":f"https://copo-project.org/api/manifest/{manifest_id}", "@type":"Dataset", "dateCreated": dateCreated, "datedModified":dateModifed} 
         manifest_item["contributor"] = []
         manifest_item["contributor"].extend( {"@id" :  p["@id"]} for p in rocrate_person.values()  )
         
         manifest_item["hasPart"] = [ { "@id": f"https://copo-project.org/api/sample/copo_id/{x['copo_id']}"} for x in samples ] 
-        manifest_item["taxonomicRange"] = [ { "@id": f"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id={x}"} for x in df["TAXON_ID"].unique() ] 
+        manifest_item["taxonomicRange"] = [ { "@id": f"http://identifiers.org/taxonomy:{x}"} for x in df["TAXON_ID"].unique() ]
+        location_mapping = [ { f"#place{str(shortuuid.ShortUUID().random(length=10))}": x} for x in df["COLLECTION_LOCATION"].unique() ]
+        manifest_item["location"] = [ { "@id": k} for element in location_mapping for k, v in element.items()]
+
         graph_list.append(manifest_item)       
         graph_list.extend(rocrate_person.values())
 
-
+        # @type: Taxon
         for x in df["TAXON_ID"].unique():
-             item = {"@id": f"http://identifiers.org/taxonomy:{x}"}
-             item["@type"] = "TAXON"
-             item["name"] = df[df["TAXON_ID"]== x ]["SCIENTIFIC_NAME"].unique()[0] if "SCIENTIFIC_NAME" in df.columns else ""
-             item["parentTaxon"] = {"@id": f"https://copo-project.org/api/sample_field/ORDER_OR_GROUP/{df[df['TAXON_ID']== x ]['ORDER_OR_GROUP'].unique()[0]}"}    
-             graph_list.append(item)
+            item = {"@id": f"http://identifiers.org/taxonomy:{x}"}
+            item["@type"] = "TAXON"
+            item["name"] = df[df["TAXON_ID"]== x ]["SCIENTIFIC_NAME"].unique()[0] if "SCIENTIFIC_NAME" in df.columns else ""
+            item["parentTaxon"] = {"@id": f"https://copo-project.org/api/sample_field/ORDER_OR_GROUP/{df[df['TAXON_ID']== x ]['ORDER_OR_GROUP'].unique()[0]}"}    
+            graph_list.append(item)
+        
+        # @type: Place
+        lat_prefix = "LATITUDE"
+        long_prefix = "LONGITUDE"
+        latLong_startEnd_lst = [f'{lat_prefix}_START',f'{lat_prefix}_END',f'{long_prefix}_START',f'{long_prefix}_END']
+
+        for element in location_mapping:
+            for key, value in element.items():
+                item = {"@id": key}
+                item["@type"] = "Place"
+                item["name"] = value
+                item["latitude"] = df[df["COLLECTION_LOCATION"] == value ]["DECIMAL_LATITUDE"].unique()[0] if "DECIMAL_LATITUDE" in df.columns else ""
+                item["logitude"] = df[df["COLLECTION_LOCATION"] == value ]["DECIMAL_LONGITUDE"].unique()[0] if "DECIMAL_LONGITUDE" in df.columns else ""
+
+                if all(element in df.columns for element in latLong_startEnd_lst):
+                    for i in latLong_startEnd_lst:
+                        item[i] = df[df["COLLECTION_LOCATION"] == value ][i].unique()[0] if i in df.columns else ""
+                                        
+                graph_list.append(item)
+                
+        # @type: BioSample
+        # Function to insert a key, value pair into a dictionary at a specified position/index
+        insert = lambda _dict, obj, pos: {k: v for k, v in (list(_dict.items())[:pos] + list(obj.items()) + list(_dict.items())[pos:])}
 
         for x in samples:
             sample_item = { "@id": f"https://copo-project.org/api/sample/copo_id/{x['copo_id']}", "@type":"BioSample" }
-            if "TAXON_ID" in x:
-              sample_item["taxonomicRange"] = {"@id": f"http://identifiers.org/taxonomy:{x['TAXON_ID']}"} 
+            keys_lst = list(x.keys())
             biosampleAccession = x.get("biosampleAccession","")
-            if biosampleAccession:
-                sample_item["biosample_identifier"] = {"@id": f"http://identifiers.org/biosample:{biosampleAccession}"} 
 
+            if "TAXON_ID" in x:
+                # sample_item["taxonomicRange"] = {"@id": f"http://identifiers.org/taxonomy:{x['TAXON_ID']}"}
+                index = keys_lst.index("TAXON_ID") + 1 # Get index/position to insert new key, value into sample item dictionary
+                x = insert(x, {'taxonomicRange': {"@id": f"https://identifiers.org/taxonomy:{x['TAXON_ID']}"}}, index) # Insert new key, value into dictionary at specified position
+            
+            if biosampleAccession:
+                sample_item["identifier"] = {"@id": f"http://identifiers.org/biosample:{biosampleAccession}"}
 
             for p in "COLLECTOR:COLLECTED_BY", "SAMPLE_COORDINATOR:SAMPLE_COORDINATOR", "PERSERVER:PERSERVED_BY", "IDENTIFIER:IDENTIFIED_BY":
                 pp = p.split(":")
                 collectors = x.get(pp[1],"")
                 if collectors:
-                    sample_item[pp[0].lower()] = []
+                    # sample_item[pp[0].lower()] = []
+                    keys_lst = list(x.keys()) # Recalculate the list of keys since it has been adjusted since a key, value pair insertion was done
+                                        
+                    field = ""
+                    if (pp[0].lower() != "collector"):
+                        field = "custodian"
+                        index = keys_lst.index(f"{pp[0]}_AFFILIATION") + 1 # Get index/position to insert new key, value pair into sample item dictionary
+                    else:
+                        field = pp[0].lower()
+                        index = keys_lst.index(pp[1]) + 1 # Get index/position to insert new key, value pair into sample item dictionary
+
+                    # Insert new key, value into dictionary at specified position
+                    x = insert(x, {field: []}, index) 
+
                     for c in collectors.split("|"):
                         collector = rocrate_person.get(c.strip(), dict())
                         if collector:
-                            sample_item[pp[0].lower()].append({"@id": collector["@id"]})
+                            # sample_item[pp[0].lower()].append({"@id": collector["@id"]})
+                            x[field].append({"@id": collector["@id"]})
         
             sample_item.update(x)
             graph_list.append(sample_item) 
 
         rocrate_json["@graph"] = graph_list
-        result_list.append(rocrate_json)
+        # result_list.append(rocrate_json)
     return rocrate_json
 
 
